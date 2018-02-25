b20670b8ffb87cca338593b28b4036bd9a1222c8
angz47 <32898793+angz47@users.noreply.github.com>
2018-02-25 23:36:01 +0700

    modul3

---
+++ app/src/main/java/com/darbel/akbar_120215006_modul3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.darbel.akbar_120215006_modul3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
